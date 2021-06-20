6965e55f8ca36160d686a252e84bb9a5e4649812
sinaloid <sinaloid@gmail.com>
2021-06-20 18:09:08 +0000

    Refactoiring, modification view,  blade compare.blade

--- resources/views/pages/compare.blade.php
+++ resources/views/pages/compare.blade.php
@@ -20,8 +20,8 @@
 
     <link href="{{ asset('/css/sin.css') }}" rel="stylesheet">
 
-    <!--<link rel="stylesheet" href="{{ asset('/dist/css/bootstrap-select.min.css') }}">
-      <script src="{{ asset('/dist/css/bootstrap-select.min.js') }}"></script>
+      <!-- <link rel="stylesheet" href="{{ asset('/dist/css/bootstrap-select.min.css') }}
+      <script src="{{ asset('/dist/js/bootstrap-select.min.js') }}"></script>
       <script src="{{ asset('/dist/js/i18n/defaults-*.min.js') }}"></script> -->
 
     <!-- jQuery library -->
@@ -38,8 +38,8 @@
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
 
-    <!-- (Optional) Latest compiled and minified JavaScript translation files -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/i18n/defaults-*.min.js"></script>
+    <!-- (Optional) Latest compiled and minified JavaScript translation files 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/i18n/defaults-*.min.js"></script> -->
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
