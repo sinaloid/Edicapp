b5ca7d6393ee6b6d0d81cf24e50655a8e04c158a
Sinali Dionou <sinaloid@gmail.com>
2021-10-15 14:48:32 +0100

    correction déploiement de laravel passport après correction update 5

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,6 +27,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
+        Passport::loadKeysFrom(__DIR__.'/../secrets/oauth');
         //
     }
 }
