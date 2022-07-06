52a973aed562ac7eb0f437924e27f2ccd3c53bd1
Sinali Dionou <sinaloid@gmail.com>
2022-07-06 12:05:33 +0000

    correction db 1

--- app/Http/Controllers/SiteUrl.php
+++ app/Http/Controllers/SiteUrl.php
@@ -78,7 +78,7 @@ class SiteUrl extends Controller
     
     public function index(Request $request)
     {
-        $commune = isset($request->comme) ? $request->commune : '';
+        $commune = isset($request->commune) ? $request->commune : '';
         $commune_id = Commune::where('commune_name',$commune)->first();
         $commune_id = isset($commune_id) ? $commune_id->id : '';
         $dataCommune = Data::where([
